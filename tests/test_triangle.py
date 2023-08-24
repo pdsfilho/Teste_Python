946a8b953a296990c665930325b9aaceada96564
pdsfilho <p.dantas@outlook.com>
2023-08-24 19:54:29 -0300

    Update test_triangle.py

--- tests/test_triangle.py
+++ tests/test_triangle.py
@@ -1,11 +1,14 @@
 import pytest
 from src.triangle import triangle_type
 
+def test_not_a_triangle():
+    assert triangle_type(2,3,10) == 'Not a triangle'
+
 def test_triangle_is_equilateral():
     assert triangle_type(5,5,5) == 'Equilateral'
 
 def test_triangle_is_isosceles():
     assert triangle_type(7,3,7) == 'Isosceles'
 
-def test triangle_is_scalene():
+def test_triangle_is_scalene():
     assert triangle_type(2,5,9) == 'Scalene'
